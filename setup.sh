ba83da0cf0cf6105b3858abffda89a1db288caae
Seunghoon Ko <imskojs@gmail.com>
2019-02-13 16:58:27 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo apt purge -y nvidia*
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/410.93/NVIDIA-Linux-x86_64-410.93.run
+wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
+
+sudo chmod +x NVIDIA-Linux-x86_64-410.93.run
+sudo sh NVIDIA-Linux-x86_64-410.93.run -q -a -n -X -s
+
+sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
+sudo git clone -b linux https://github.com/imskojs/ccminer.git
+cd ccminer/
+sudo ./build.sh
